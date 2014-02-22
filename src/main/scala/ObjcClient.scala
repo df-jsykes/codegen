45ac2282a27ad9d990b5ea71627a2bb10bcde4be
Jason Sykes <jasonsykes@dreamfactory.com>
2014-02-21 19:05:03 -0500

    initial commit

---
+++ src/main/scala/ObjcClient.scala
@@ -0,0 +1,23 @@
+import com.wordnik.swagger.codegen.BasicObjcGenerator
+
+object ObjcClient extends BasicObjcGenerator {
+  def main(args: Array[String]) = generateClient(args)
+
+  // where to write generated code
+  override def destinationDir = "samples/client/objc/client"
+
+  // to avoid recompiling ...
+  override def templateDir = "src/main/resources/objc"
+
+  // supporting classes
+  override def supportingFiles =
+    List(
+      ("NIKSwaggerObject.h", destinationDir, "NIKSwaggerObject.h"),
+      ("NIKSwaggerObject.m", destinationDir, "NIKSwaggerObject.m"),
+      ("NIKApiInvoker.h", destinationDir, "NIKApiInvoker.h"),
+      ("NIKApiInvoker.m", destinationDir, "NIKApiInvoker.m"),
+      ("NIKFile.h", destinationDir, "NIKFile.h"),
+      ("NIKFile.m", destinationDir, "NIKFile.m"),
+      ("NIKDate.h", destinationDir, "NIKDate.h"),
+      ("NIKDate.m", destinationDir, "NIKDate.m"))
+}
