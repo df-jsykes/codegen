9df1d62ac18205ebec990abbcfd2e41ca1971043
Tony Tam <fehguy@gmail.com>
2014-02-28 09:53:56 -0800

    added sample app

---
+++ android-app/app/src/main/java/com/dreamfactory/model/Record.java
@@ -0,0 +1,25 @@
+package com.dreamfactory.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Record {
+  /* Array of field name-value pairs. */
+  @JsonProperty("_field_")
+  private List<String> _field_ = new ArrayList<String>();
+  public List<String> get_field_() {
+    return _field_;
+  }
+  public void set_field_(List<String> _field_) {
+    this._field_ = _field_;
+  }
+
+  @Override
+  public String toString()  {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Record {\n");
+    sb.append("  _field_: ").append(_field_).append("\n");
+    sb.append("}\n");
+    return sb.toString();
+  }
+}
+
